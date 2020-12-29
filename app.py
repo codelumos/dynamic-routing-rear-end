31fb2a20cef79f9beb744c1ae410553601dc8c66
ShenZheng <mf20320121@smail.nju.edu.cn>
2020-12-29 15:21:39 +0800

    修改获取前端数据方式

--- app.py
+++ app.py
@@ -11,19 +11,20 @@ CORS(app, supports_credentials=True)
 @app.route('/test', methods=['POST'])
 def test():
     # 获取前端传递的参数
-    data = request.get_data()
     # 转换成dict
-    data = json.loads(data.decode("utf-8"))
-    print(data)
-    return 'a'
+    data = json.loads(request.get_data())
+    print(data['pwd'])
+    return '调用完毕'
 
 # telnet登陆设备
 @app.route('/login', methods=['POST'])
 def login_host():
-    # 从form-data中获取设备编号, ip, 密码
-    dev_no = request.form['dev_no']
-    ip = request.form['ip']
-    pwd = request.form['pwd']
+    # 获取前端传递的数据
+    data = json.loads(request.get_data())
+    # 设备编号, ip, 密码
+    dev_no = data['dev_no']
+    ip = data['ip']
+    pwd = data['pwd']
 
     device = get_device(dev_no)
     # 登录成功返加true，否则返回false
@@ -40,8 +41,9 @@ def login_host():
 # telnet登出设备
 @app.route('/logout', methods=['POST'])
 def logout_host():
-    # 从form-data中获取设备编号
-    dev_no = request.form['dev_no']
+    data = json.loads(request.get_data())
+    # 获取设备编号
+    dev_no = data['dev_no']
 
     device = get_device(dev_no)
     # 登出成功返加true，否则返回false
@@ -58,8 +60,9 @@ def logout_host():
 # 查看路由表和路由协议
 @app.route('/show_info', methods=['POST'])
 def show_info():
-    # 从form-data中获取设备编号
-    dev_no = request.form['dev_no']
+    data = json.loads(request.get_data())
+    # 获取设备编号
+    dev_no = data['dev_no']
     device = get_device(dev_no)
     # 执行命令
     route = device.execute_command('show ip route')
@@ -71,10 +74,11 @@ def show_info():
 # 配置RIP动态路由
 @app.route('/config_rip', methods=['POST'])
 def config_rip():
-    # 从form-data中获取特权密码
-    en_pwd_r0 = request.form['en_pwd_r0']
-    en_pwd_r1 = request.form['en_pwd_r1']
-    en_pwd_r2 = request.form['en_pwd_r2']
+    data = json.loads(request.get_data())
+    # 获取特权密码
+    en_pwd_r0 = data['en_pwd_r0']
+    en_pwd_r1 = data['en_pwd_r1']
+    en_pwd_r2 = data['en_pwd_r2']
 
     is_succeed_r0, msg_r0 = router0.config_rip(['172.16.0.0', '172.17.0.0'], en_pwd_r0)
     is_succeed_r1, msg_r1 = router1.config_rip(['172.16.0.0', '172.17.0.0', '172.18.0.0'], en_pwd_r1)
@@ -89,13 +93,15 @@ def config_rip():
 # 配置OSPF协议
 @app.route('/config_ospf', methods=['POST'])
 def config_ospf():
-    # 从form-data中获取特权密码
-    en_pwd_r0 = request.form['en_pwd_r0']
-    en_pwd_r1 = request.form['en_pwd_r1']
-    en_pwd_r2 = request.form['en_pwd_r2']
+    data = json.loads(request.get_data())
+    # 获取特权密码
+    en_pwd_r0 = data['en_pwd_r0']
+    en_pwd_r1 = data['en_pwd_r1']
+    en_pwd_r2 = data['en_pwd_r2']
 
     is_succeed_r0, msg_r0 = router0.config_ospf(en_pwd_r0, ['172.16.0.0', '172.17.0.0'], ['0', '0'], '0.0.255.255')
-    is_succeed_r1, msg_r1 = router1.config_ospf(en_pwd_r1, ['172.16.0.0', '172.17.0.0', '172.18.0.0'], ['0', '0', '0'], '0.0.255.255')
+    is_succeed_r1, msg_r1 = router1.config_ospf(en_pwd_r1, ['172.16.0.0', '172.17.0.0', '172.18.0.0'], ['0', '0', '0'],
+                                                '0.0.255.255')
     is_succeed_r2, msg_r2 = router2.config_ospf(en_pwd_r2, ['172.16.0.0', '172.18.0.0'], ['0', '0'], '0.0.255.255')
 
     is_succeed = is_succeed_r0 and is_succeed_r1 and is_succeed_r2
