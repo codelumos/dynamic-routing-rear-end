6423c6fd00c9dc308498616137c34cfd656c1ab4
ShenZheng <mf20320121@smail.nju.edu.cn>
2020-12-30 23:34:05 +0800

    新增前后端交互用测试函数config_bgp

--- app.py
+++ app.py
@@ -23,12 +23,12 @@ def hello():
 def login_host():
     # 获取前端传递的数据
     data = json.loads(request.get_data())
+    logging.info('Login:' + str(data))
     # 设备编号, ip, 密码
     dev_no = data['dev_no']
     ip = data['ip']
     pwd = data['pwd']
 
-    logging.info('Login:' + str(data))
     device = get_device(dev_no)
     # 登录成功返加true，否则返回false
     if dev_no is not None:
@@ -45,10 +45,10 @@ def login_host():
 @app.route('/logout', methods=['POST'])
 def logout_host():
     data = json.loads(request.get_data())
+    logging.info('Logout:' + str(data))
     # 获取设备编号
     dev_no = data['dev_no']
 
-    logging.info('Logout:' + str(data))
     device = get_device(dev_no)
     # 登出成功返加true，否则返回false
     if dev_no is not None:
@@ -65,10 +65,10 @@ def logout_host():
 @app.route('/show_info', methods=['POST'])
 def show_info():
     data = json.loads(request.get_data())
+    logging.info('Show info:' + str(data))
     # 获取设备编号
     dev_no = data['dev_no']
 
-    logging.info('Show info:' + str(data))
     device = get_device(dev_no)
     # 执行命令
     route = device.execute_command('show ip route')
@@ -81,12 +81,12 @@ def show_info():
 @app.route('/config_rip', methods=['POST'])
 def config_rip():
     data = json.loads(request.get_data())
-    # 获取特权密码
-    en_pwd_r0 = data['en_pwd_r0']
-    en_pwd_r1 = data['en_pwd_r1']
-    en_pwd_r2 = data['en_pwd_r2']
-
     logging.info('Config RIP:' + str(data))
+    # 获取特权密码
+    en_pwd_r0 = data['pwd_r0']
+    en_pwd_r1 = data['pwd_r1']
+    en_pwd_r2 = data['pwd_r2']
+
     is_succeed_r0, msg_r0 = router0.config_rip(['172.16.0.0', '172.17.0.0'], en_pwd_r0)
     is_succeed_r1, msg_r1 = router1.config_rip(['172.16.0.0', '172.17.0.0', '172.18.0.0'], en_pwd_r1)
     is_succeed_r2, msg_r2 = router2.config_rip(['172.16.0.0', '172.18.0.0'], en_pwd_r2)
@@ -101,12 +101,12 @@ def config_rip():
 @app.route('/config_ospf', methods=['POST'])
 def config_ospf():
     data = json.loads(request.get_data())
+    logging.info('Config OSPF:' + str(data))
     # 获取特权密码
     en_pwd_r0 = data['en_pwd_r0']
     en_pwd_r1 = data['en_pwd_r1']
     en_pwd_r2 = data['en_pwd_r2']
 
-    logging.info('Config OSPF:' + str(data))
     is_succeed_r0, msg_r0 = router0.config_ospf(en_pwd_r0, ['172.16.0.0', '172.17.0.0'], ['0', '0'], '0.0.255.255')
     is_succeed_r1, msg_r1 = router1.config_ospf(en_pwd_r1, ['172.16.0.0', '172.17.0.0', '172.18.0.0'], ['0', '0', '0'],
                                                 '0.0.255.255')
@@ -114,6 +114,17 @@ def config_ospf():
 
     is_succeed = is_succeed_r0 and is_succeed_r1 and is_succeed_r2
     msg = msg_r0 + ', ' + msg_r1 + ', ' + msg_r2
+    result = {'state': is_succeed, 'msg': msg}
+    return jsonify(result)
+
+
+@app.route('/config_bgp', methods=['POST'])
+def config_bgp():
+    data = json.loads(request.get_data())
+    is_succeed = True
+    msg = '配置完成'
+    # is_succeed = False
+    # msg = '配置失败'
     result = {'state': is_succeed, 'msg': msg}
     return jsonify(result)
 
